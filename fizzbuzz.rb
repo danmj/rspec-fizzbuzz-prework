db3a6908e59cdc357951503d66a9b3b987548403
danmj <daniel.jung@stern.nyu.edu>
2018-08-14 18:24:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(int)
   
   if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   end  
   
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   end 
     
   elsif int % 5 == 0
-    return "Buzz"
+    "Buzz"
   end
   
   else
