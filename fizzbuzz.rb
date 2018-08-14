4c0fda81ca98ae46989e52fb29cd992bb2ad8579
danmj <daniel.jung@stern.nyu.edu>
2018-08-14 18:10:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,14 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-    end
-  
-  elsif int % 5 == 0
-    "Buzz"
-    end 
-    
-  elsif int % 3 == 0 && int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
     end  
+  
+  elsif int % 3 == 0
+    "Fizz"
+    end 
     
+  elsif int % 5 == 0
+    "Buzz"
   else
     nil
     end
